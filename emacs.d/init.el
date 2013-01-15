98e4e5e4da15ea8e1d4c0e4b82df7275240aceb2
Mark Kornblum <mark.kornblum@gmail.com>
2013-01-15 14:07:34 -0800

    better emacs buffer rotation function courtesy of re5et

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -44,27 +44,21 @@
   (save-excursion)
   (indent-region (point-min) (point-max)))
 
-(defun rotate-windows ()
-  "Rotate your windows"
+(defun rotate-frame-window-buffers ()
   (interactive)
-  (cond
-   ((not (> (count-windows) 1))
-    (message "You can't rotate a single window!"))
-   (t
-    (let ((i 1)
-          (num-windows (count-windows)))
-      (while  (< i num-windows)
-        (let* ((w1 (elt (window-list) i))
-               (w2 (elt (window-list) (+ (% i num-windows) 1)))
-               (b1 (window-buffer w1))
-               (b2 (window-buffer w2))
-               (s1 (window-start w1))
-               (s2 (window-start w2)))
-          (set-window-buffer w1 b2)
-          (set-window-buffer w2 b1)
-          (set-window-start w1 s2)
-          (set-window-start w2 s1)
-          (setq i (1+ i))))))))
+  (let ((map
+         (mapcar
+          (lambda (window)
+            `(,window
+              ,(window-buffer
+                (next-window window))))
+          (window-list))))
+    (mapcar
+     (lambda (window-to-buffer)
+       (let ((window (car window-to-buffer))
+             (buffer (cadr window-to-buffer)))
+         (select-window window)
+         (switch-to-buffer buffer))) map)))
 
 (defun auto-make-directory ()
   (let ((dir (file-name-directory (buffer-file-name))))
@@ -144,7 +138,7 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-,") 'indent-buffer)
-(global-set-key (kbd "C-S-r") 'rotate-windows)
+(global-set-key (kbd "C-S-r") 'rotate-frame-window-buffers)
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (setq
