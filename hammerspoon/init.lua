004e754e5d2c0bd00182c41db4c150b717eed6ab
Mark Kornblum <mark.kornblum@gmail.com>
2017-08-27 23:05:49 -0700

    Add global mic mute hotkey

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,58 +1,8 @@
 hs.window.animationDuration = 0 -- disable animations
+require('microphone')
+require('windows')
 
-function getMaxFrame()
-  local win = hs.window.focusedWindow()
-  local screen = win:screen()
-  return screen:frame()
-end
-
-function resizeWindow(options)
-  local options = options or {}
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local max = getMaxFrame()
-
-  f.x = options.x or max.x
-  f.y = options.y or max.y
-  f.w = options.width or max.w
-  f.h = options.height or max.h
-  win:setFrame(f)
-end
-
-windowMover = hs.hotkey.modal.new({"control"}, "space", "Resize Window")
-
-function windowMover:exited()
-  hs.alert.show("Resize Canceled")
-end
-
-windowMover:bind('', 'escape', function()
-                   windowMover:exit()
-end)
-
-windowMover:bind('ctrl', 'g', function()
-                   windowMover:exit()
-end)
-
-windowMover:bind("", "Down", function()
-                   resizeWindow()
-                   windowMover:exit()
-end)
-
-windowMover:bind("", "Left", function()
-                   local max = getMaxFrame()
-                   newWidth = max.w / 2
-                   resizeWindow({ width = newWidth })
-                   windowMover:exit()
-end)
-
-windowMover:bind("", "Right", function()
-                   local max = getMaxFrame()
-                   newWidth = max.w / 2
-                   newX = max.x + (max.w / 2)
-                   resizeWindow({ width = newWidth, x = newX })
-                   windowMover:exit()
-end)
-
+local message = require('status-message')
 
 function reloadConfig(files)
   doReload = false
@@ -66,4 +16,4 @@ function reloadConfig(files)
   end
 end
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
+message.new("Config loaded"):notify()
---
+++ hammerspoon/windows.lua
@@ -0,0 +1,52 @@
+function getMaxFrame()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+  return screen:frame()
+end
+
+function resizeWindow(options)
+  local options = options or {}
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local max = getMaxFrame()
+
+  f.x = options.x or max.x
+  f.y = options.y or max.y
+  f.w = options.width or max.w
+  f.h = options.height or max.h
+  win:setFrame(f)
+end
+
+windowMover = hs.hotkey.modal.new({"command"}, "return", "Resize Window")
+
+function windowMover:exited()
+  hs.alert.show("Resize Complete")
+end
+
+windowMover:bind('', 'escape', function()
+                   windowMover:exit()
+end)
+
+windowMover:bind('ctrl', 'g', function()
+                   windowMover:exit()
+end)
+
+windowMover:bind("", "Down", function()
+                   resizeWindow()
+                   windowMover:exit()
+end)
+
+windowMover:bind("", "Left", function()
+                   local max = getMaxFrame()
+                   newWidth = max.w / 2
+                   resizeWindow({ width = newWidth })
+                   windowMover:exit()
+end)
+
+windowMover:bind("", "Right", function()
+                   local max = getMaxFrame()
+                   newWidth = max.w / 2
+                   newX = max.x + (max.w / 2)
+                   resizeWindow({ width = newWidth, x = newX })
+                   windowMover:exit()
+end)
